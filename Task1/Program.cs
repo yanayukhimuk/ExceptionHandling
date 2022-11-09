3ec6c1a852e12b6045f78bae6795c33d68353589
yanayukhimuk <yana.ryzhikova.brest.belarus@gmail.com>
2022-11-09 16:16:14 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string answer;
+            do
+            {
+                try
+                {
+                    string line = GetLine();
+                    char firstLetter = GetFirstCharacter(line);
+                    Console.WriteLine("The first letter is: " + firstLetter);
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message + " Probably, the string is empty.");
+                }
+                catch (NullReferenceException e)
+                {
+                    Console.WriteLine(e.Message + " Probably, the string is null.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message + " An unknown error has occured.");
+                }
+
+                Console.WriteLine("Do you want to try again? (yes/no) ");
+                answer = Console.ReadLine();
+            }
+            while (answer != "no");
+        }
+
+        public static char GetFirstCharacter(string line)
+        {
+            char firstCharacter;
+            if (line == null)
+            {
+                throw new NullReferenceException();
+            }
+            else if (line.Length <= 0)
+                throw new ArgumentOutOfRangeException();
+            else
+                firstCharacter = line[0];
+            return line[0];
+        }
+
+        public static string GetLine()
+        {
+            Console.WriteLine("Enter any line: ");
+            string enteredString = Console.ReadLine();
+            return enteredString;
+        }
+    }
+}
---
+++ Task2/NumberParser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        public int Parse(string stringValue)
+        {
+            if (stringValue == null)
+            {
+                throw new ArgumentNullException(nameof(stringValue), "String cannot be null, empty or whitespace");
+            }
+
+            if (string.IsNullOrEmpty(stringValue) || string.IsNullOrWhiteSpace(stringValue))
+            {
+                throw new FormatException($"Symbol '{stringValue}' cannot be part of a number");
+            }
+
+            stringValue = stringValue.Trim();
+
+            var isNegative = IsNegative(stringValue);
+            var hasPlus = HasPlus(stringValue);
+
+            var result = 0;
+            var start = isNegative || hasPlus ? 1 : 0;
+
+            for (var i = start; i < stringValue.Length; i++)
+            {
+                if (stringValue[i] < '0' || stringValue[i] > '9')
+                {
+                    throw new FormatException($"Symbol '{stringValue[i]}' cannot be part of a number");
+                }
+
+                try
+                {
+                    result = checked(result * 10 + stringValue[i] - '0');
+                }
+                catch (OverflowException)
+                {
+                    throw new OverflowException($"Can't be converted, because number must be between {int.MinValue} and {int.MaxValue}");
+                }
+            }
+
+            return isNegative ? -result : result;
+        }
+
+        private static bool IsNegative(string value) => value[0] == '-';
+        private static bool HasPlus(string value) => value[0] == '+';
+    }
+}
